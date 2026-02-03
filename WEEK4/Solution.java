b48b9f10844bc48096b5c32d2a7502aa8abb76c8
DHIVYA SV <vtu26230@veltech.edu.in>
2026-02-03 22:41:23 +0530

    Create Solution.java

---
+++ WEEK4/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n= s.length();
+        int mid= n/2;
+        int c1=0;
+        int c2=0;
+        for (int i=0; i<mid; i++){
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                c1+=1;
+            }
+        }
+        for (int i=mid; i<n; i++){
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                c2+=1;
+            }
+        }
+        if (c1==c2)
+            return true;
+        return false;
+    }
+}
